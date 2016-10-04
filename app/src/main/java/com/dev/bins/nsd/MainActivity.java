15d98d555165a663c5394093a83e1678088b4042
bin <bin6160@126.com>
2016-10-04 14:27:29 +0800

    register server

--- app/src/main/java/com/dev/bins/nsd/MainActivity.java
+++ app/src/main/java/com/dev/bins/nsd/MainActivity.java
@@ -1,13 +1,49 @@
 package com.dev.bins.nsd;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button mBtnSend;
+    private EditText mEtContent;
+    private Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        mBtnSend = (Button) findViewById(R.id.btn_send);
+        mEtContent = (EditText) findViewById(R.id.et_content);
+        mBtnSend.setOnClickListener(this);
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- nsdserver/src/main/java/com/dev/bins/nsdserver/MainActivity.java
+++ nsdserver/src/main/java/com/dev/bins/nsdserver/MainActivity.java
@@ -1,13 +1,127 @@
 package com.dev.bins.nsdserver;
 
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, Runnable {
+
+    public static final String SERVICE_NAME = "NSDService";
+    public static final String SERVICE_TYPE = "_http._tcp.";
+    private Button mBtnRegister;
+    private TextView mTvContent;
+    private NsdServiceInfo mNsdServiceInfo;
+    private ServerSocket mServerSocket;
+    private int mPort;
+    private NsdManager mNsdManager;
+    private NsdManager.RegistrationListener mRegistrationListener;
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            mTvContent.setText(mTvContent.getText() + (String) msg.obj);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTvContent = (TextView) findViewById(R.id.tv_content);
+        mBtnRegister = (Button) findViewById(R.id.btn_register);
+        mBtnRegister.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        createServerSocket();
+        createNsdServiceInfo();
+        createRegistration();
+        register();
+    }
+
+    private void register() {
+        mNsdManager = (NsdManager) getSystemService(NSD_SERVICE);
+        mNsdManager.registerService(mNsdServiceInfo, NsdManager.PROTOCOL_DNS_SD, mRegistrationListener);
+    }
+
+    private void createRegistration() {
+        mRegistrationListener = new NsdManager.RegistrationListener() {
+            @Override
+            public void onRegistrationFailed(NsdServiceInfo nsdServiceInfo, int i) {
+                Toast.makeText(MainActivity.this, "onRegistrationFailed", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onUnregistrationFailed(NsdServiceInfo nsdServiceInfo, int i) {
+                Toast.makeText(MainActivity.this, "onUnregistrationFailed", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onServiceRegistered(NsdServiceInfo nsdServiceInfo) {
+                Toast.makeText(MainActivity.this, "onServiceRegistered", Toast.LENGTH_SHORT).show();
+                new Thread(MainActivity.this).start();
+            }
+
+            @Override
+            public void onServiceUnregistered(NsdServiceInfo nsdServiceInfo) {
+                Toast.makeText(MainActivity.this, "onServiceUnregistered", Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+
+    /**
+     * 创建一个server来获取端口
+     */
+    private void createServerSocket() {
+        if (mServerSocket != null) return;
+        try {
+            mServerSocket = new ServerSocket(0);//设为0,会自动获取没有占用的端口
+            mPort = mServerSocket.getLocalPort();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 创建NsdServiceInfo
+     */
+    private void createNsdServiceInfo() {
+        if (mNsdServiceInfo != null) return;
+        mNsdServiceInfo = new NsdServiceInfo();
+        mNsdServiceInfo.setServiceName(SERVICE_NAME);
+        mNsdServiceInfo.setServiceType(SERVICE_TYPE);
+        mNsdServiceInfo.setPort(mPort);
+    }
+
+    @Override
+    public void run() {
+        try {
+            Socket socket = mServerSocket.accept();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String content = null;
+            while ((content = bufferedReader.readLine()) != null) {
+                Message msg = mHandler.obtainMessage();
+                msg.obj = content;
+                msg.what = 0;
+                mHandler.sendMessage(msg);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
